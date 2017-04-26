177055bc92264944da5595e7dc3d5aac38547b13
sergiu99 <sergiu.ifimov@gmail.com>
2017-04-26 13:01:40 -0400

    login function

--- pages/login/userLogin.php
+++ pages/login/userLogin.php
@@ -1,13 +1,44 @@
 <?php
-	/* This page is used to login a user to the data base */
+/* This page is used to login a user to the data base */
 	
 	if(isset($_POST["email"]) && isset($_POST["pwd"]))
 	{
-		/* TODO : access datbase and check if email and pass are valid */
-		
-		/* for now, the echo will be success only */
-		echo "success";
-	}
+			
+
+	$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "universaldb";
+       // $name = $_POST["email"]; 
+       // $password = $_POST["pwd"]; 
+	   
+	    $name = "admin"; 
+        $password = "admin"; 
+// Create connection
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT username, userId, userType FROM users WHERE username = '".$name."' AND  password = '".$password."'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+	$row = $result->fetch_assoc();
+	
+	// GET ALL THE USER INFOS AND STORE THEM IN THE SESSION
+    session_start();
+    $_SESSION["user_id"] =  $row["userId"];
+	$_SESSION["user_type"] =  $row["userType"];
+	$_SESSION["username"] =  $row["userType"];
+	 echo "session started";
+	
+} else {
+    echo "wrong user password combo";
+}
+$conn->close();
 	
 	else
 	{
