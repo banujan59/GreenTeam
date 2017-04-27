8f272f04496662083fea23423c2c676cf537783f
sergiu99 <sergiu.ifimov@gmail.com>
2017-04-27 13:37:50 -0400

    1

--- pages/login/userLogin.php
+++ pages/login/userLogin.php
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT username, userId, userType, balance FROM users WHERE username = '".$name."' AND  password = '".$password."'";
+$sql = "SELECT username, userId, userType FROM users WHERE username = '".$name."' AND  password = '".$password."'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -33,7 +33,6 @@ if ($result->num_rows > 0) {
     session_start();
     $_SESSION["user_email"] =  $row["userId"];
 	$_SESSION["user_type"] =  $row["userType"];
-	$_SESSION["user_balance"] =  $row["balance"];
 	$_SESSION["username"] =  $row["userType"];
 	
 echo "success";
